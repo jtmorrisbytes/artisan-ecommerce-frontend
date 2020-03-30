c94121250c185300b1022b1b8a643dc6ccf7167d
= <=>
2020-03-30 12:50:29 -0500

    Scaffold search component

--- src/controllers/Header/Header.js
+++ src/controllers/Header/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Navigation from "../Navigation/Navigation";
+import Search from "../Search/Search";
 const Header = props => {
   return (
     <header className="Header">
       <Navigation />
+      <Search />
     </header>
   );
 };
---
+++ src/controllers/Search/Search.js
@@ -0,0 +1,6 @@
+import React from React
+import "./Search.css"
+const Search = (props) => {
+  return (<div className="Search"></div>);
+}
+export default Search
