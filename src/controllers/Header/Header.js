b68ad22dd5bf8511030f77b1472bb8624f320fd5
= <=>
2020-03-30 12:43:45 -0500

    scaffolded Navigation component

---
+++ src/controllers/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default Header = props => {
+  return (
+    <header className="Header">
+      <Navigation />
+    </header>
+  );
+};
---
+++ src/controllers/example-functional-component.js
@@ -0,0 +1,6 @@
+import React from React
+import "./{blank}.css"
+
+export default {Blank} = (props) {
+  return (<div></div>);
+}
